2864275ce0177ebbd02cfdd35c5219b536ccef9d
Gus  Cantieni <gus.cantieni@gmail.com>
2025-10-05 10:56:53 -0400

    nvim overhaul

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -21,32 +21,40 @@ if status is-interactive
     # Run keychain to load ssh keys
     keychain --eval --agents ssh id_rsa -q | source
 
+    # This command leads to incredibly long loading time as it indexes my
+	
+	# entire home directory.
+	# This just turns it off.
+		complete -c dots -f
+
     # Emacs vterm integration
-    function vterm_printf;
-        if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
-            # tell tmux to pass the escape sequences through
-            printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
-        else if string match -q -- "screen*" "$TERM"
-            # GNU screen (screen, screen-256color, screen-256color-bce)
-            printf "\eP\e]%s\007\e\\" "$argv"
-        else
-            printf "\e]%s\e\\" "$argv"
+ if [ "$INSIDE_EMACS" = 'vterm' ]  
+        function vterm_printf;
+            if begin; [  -n "$TMUX" ]  ; and  string match -q -r "screen|tmux" "$TERM"; end
+                # tell tmux to pass the escape sequences through
+                printf "\ePtmux;\e\e]%s\007\e\\" "$argv"
+            else if string match -q -- "screen*" "$TERM"
+                # GNU screen (screen, screen-256color, screen-256color-bce)
+                printf "\eP\e]%s\007\e\\" "$argv"
+            else
+                printf "\e]%s\e\\" "$argv"
+            end
         end
-    end
 
-    function vterm_prompt_end;
-        vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
-    end
+        function vterm_prompt_end;
+            vterm_printf '51;A'(whoami)'@'(hostname)':'(pwd)
+        end
 
-    functions --copy fish_prompt vterm_old_fish_prompt
+        functions --copy fish_prompt vterm_old_fish_prompt
 
-    function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
-        # Remove the trailing newline from the original prompt. This is done
-        # using the string builtin from fish, but to make sure any escape codes
-        # are correctly interpreted, use %b for printf.
-        printf "%b" (string join "\n" (vterm_old_fish_prompt))
-        vterm_prompt_end
-    end
+        function fish_prompt --description 'Write out the prompt; do not replace this. Instead, put this at end of your file.'
+            # Remove the trailing newline from the original prompt. This is done
+            # using the string builtin from fish, but to make sure any escape codes
+            # are correctly interpreted, use %b for printf.
+            printf "%b" (string join "\n" (vterm_old_fish_prompt))
+            vterm_prompt_end
+        end
+  end
 
     alias lex="ssh -L 11000:gcantieni-l2.abinitio.com:4000 gcantieni@ssh-lex.abinitio.com"
     alias rdp="ssh -i /Users/gus/.ssh/id_rsa -L 12000:gcantieni-l2.abinitio.com:3389 gcantieni@ssh-lex.abinitio.com"
